7c5094e30d3a6b3ece2dfbd47500bc14decfb99d
hudehyan <65912531+hudehyan@users.noreply.github.com>
2021-01-18 13:57:13 -0500

    step 5

--- functions.js
+++ functions.js
@@ -4,6 +4,17 @@ function calculatetotal(quantity,price)
     return price*quantity;
 };
 function outputCartRow(file,title,quantity,price,total)
-{}
+{
+    total = calculateTotal(quantity[i],price[i]);
+    document.write("<tr>");
+    document.write("<td><img src='images/"+ file[i] + "'</td>'");
+    document.write("<td>" + title[i] +"</td>");
+    document.write("<td>" +quantity[i] + "</td>");
+    document.write("<td>" + price[i].toFixed(2) + "</td>");
+    document.write("<td>" + total.toFixed(2) + "</td");
+    document.write("</tr");
+
+
+};
 
         
